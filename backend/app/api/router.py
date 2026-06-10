d70a1bd04cd32b907215da1d27ab39268d08b7bf
qiau <raihanekap04@gmail.com>
2026-06-10 21:28:09 +0700

    fix router.py in backend

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -2,10 +2,8 @@ from fastapi import APIRouter
 
 from app.api.routers import auth
 from app.api.routers.user import analysis_histories
-from app.api.routers.admin import datasets
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(analysis_histories.router)
-api_router.include_router(datasets.router)
